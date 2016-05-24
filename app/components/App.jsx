69a2781881d862e615d6d2ed808a3164b7d958e2
JasonBai007 <byygkcg@163.com>
2016-05-24 09:23:14 +0800

    添加fetch

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -5,35 +5,46 @@ import 'whatwg-fetch';
 import './App.css';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            text:'初始态',
+            arr:['请选择']
+        }
+    }
 
-  getData = () => {
-    let data = {a:23};
-    fetch('../data/selectData.json',{
-        method:'get',
-         headers: {
-           "Content-Type": "application/json"
-         },
-         credentials: "include"
-    })
-        .then((res) => {
-            console.log(res);
-            return res.json();
-        })
-        .then((data) => {
-            console.log(data.obj);          
-        })
-        .catch((ex) => {
-            console.log(ex);
-        });
-  }
+    componentDidMount() {
+        fetch('../data/selectData.json')
+            .then((res) => {
+                console.log(res);
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data.obj);  
+                this.setState({arr:data.obj});        
+            })
+            .catch((ex) => {
+                console.log(ex);
+            });
+    }
 
-  render() {
-    return (
-      <h1 onClick={this.getData}>Hello World</h1>
-    );
-  }
+    getData = () => {      
+        this.setState({text:'激发态'});        
+    }
+
+    render() {
+      return (
+        <div>
+          <h1 onClick={this.getData}>单击我：{this.state.text}</h1>
+          <select>
+            {
+              this.state.arr.map((v,i) => {
+                return <option key={i}>{v}</option>
+              })
+            }
+          </select>
+        </div>
+      );
+    }
 }
 
