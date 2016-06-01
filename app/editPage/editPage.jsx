32b3512e1eb1a9a9b4aeb6b1189bfe43cfa3ec3e
JasonBai007 <byygkcg@163.com>
2016-06-01 11:25:55 +0800

    测试

--- app/editPage/editPage.jsx
+++ app/editPage/editPage.jsx
@@ -13,7 +13,7 @@ export default class EditPage extends React.Component {
       return (
         <div>
             <Title titleName="编辑广告系列"/>
-            <i className="fa fa-cog fa-spin fa-3x fa-fw"></i>          
+            <EditForm />          
         </div>
       );
     }
