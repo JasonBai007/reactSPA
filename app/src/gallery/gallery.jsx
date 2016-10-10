51e20f2ec6b51f84887c700fb21379e5b17c1e77
JasonBai007 <byygkcg@163.com>
2016-10-10 16:11:27 +0800

    添加reflux

--- app/src/gallery/gallery.jsx
+++ app/src/gallery/gallery.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import Reflux from 'reflux';
 import './gallery.less';
+
+var actions = Reflux.createActions([
+    'increment'
+]);
+
+var myStore = Reflux.createStore({
+    listenables: actions,
+    init() {
+        this.state = {count:0}
+    },
+    onIncrement() {
+        console.log('我被执行了！');
+        this.state.count++;
+        this.trigger(this.state);
+    }
+});
 
 export default class Gallery extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-        }               
+        this.state = {};
+        this.store = myStore;               
+    }
+    handleClick = () => {
+        actions.increment();
     }
     render() {        
         return (
             <div>
-                <i className="fa fa-cog fa-spin fa-3x fa-fw"></i>
+                <button onClick={this.handleClick}>单击增加数值</button>
+                <p>{this.state.count}</p>
             </div>
         )
     }       
