115fccee770e51e87c4f884f0b313b651a1db1d1
JasonBai007 <byygkcg@163.com>
2016-10-18 09:25:06 +0800

    配置生产环境

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -34,6 +34,11 @@ module.exports = {
             compress: {
                 warnings: false
             }
+        }),
+        new webpack.DefinePlugin({
+          "process.env": { 
+             NODE_ENV: JSON.stringify("production") 
+           }
         })
     ]
 };
