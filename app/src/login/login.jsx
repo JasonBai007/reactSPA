cf4ce303e9babce269d5149ebdf1b0c6979d2337
JasonBai007 <byygkcg@163.com>
2016-10-19 20:34:14 +0800

    添加密码提示

--- app/src/login/login.jsx
+++ app/src/login/login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { browserHistory } from 'react-router'
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, notification} from 'antd';
 
 import './login.less'
 
@@ -17,9 +17,25 @@ class LoginPage extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Received values of form:', this.props.form.getFieldsValue());
-        // 表单的路由处理
-        browserHistory.push('/');
+        let n = this.props.form.getFieldsValue().username;
+        let p = this.props.form.getFieldsValue().password;
+        if (n === 'ilovejasonbai' && p === 'ilovejasonbai') {
+            // 表单的路由处理
+            browserHistory.push('/');            
+        }
+    }
+
+    // 返回一个弹框对象
+    openNotificationWithIcon = (type) => {
+        return notification[type]({
+                 message: '用户名&密码',
+                 description: '都是：ilovejasonbai',
+                 duration: 6
+               })
+    }
+
+    componentDidMount() {
+        this.openNotificationWithIcon('info');  
     }
 
     render() {  
@@ -30,7 +46,7 @@ class LoginPage extends React.Component {
                 <div id="loginWrap">                
                     <Form horizontal onSubmit={this.handleSubmit}>
                         <FormItem>
-                            <Input placeholder="Username" {...getFieldProps('userName')} />
+                            <Input placeholder="Username" {...getFieldProps('username')} />
                         </FormItem>
                         <FormItem>
                             <Input placeholder="Password" {...getFieldProps('password')} />
